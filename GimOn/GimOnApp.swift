bf7f8dbed951597d0847193cc49a49b9a5eaddef
Enjel Hutasoit <enjel.hutasoit@gmail.com>
2025-07-31 20:12:47 +0700

    Initial Commit

---
+++ GimOn/GimOnApp.swift
@@ -0,0 +1,17 @@
+//
+//  GimOnApp.swift
+//  GimOn
+//
+//  Created by Enjel Hutasoit on 31/07/25.
+//
+
+import SwiftUI
+
+@main
+struct GimOnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
