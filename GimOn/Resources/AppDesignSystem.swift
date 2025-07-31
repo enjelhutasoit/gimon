073b6dcf7baf5f528511e9577a56100c4517928e
Enjel Hutasoit <enjel.hutasoit@gmail.com>
2025-07-31 20:36:32 +0700

    Add custom Fonts

---
+++ GimOn/Resources/AppDesignSystem.swift
@@ -0,0 +1,29 @@
+//
+//  AppDesignSystem.swift
+//  GimOn
+//
+//  Created by Enjel Hutasoit on 31/07/25.
+//
+
+import Foundation
+
+struct AppFonts {
+    static let light = "Outfit-Light"
+    static let extraLight = "Outfit-ExtraLight"
+    static let thin = "Outfit-Thin"
+    static let regular = "Outfit-Regular"
+    static let medium = "Outfit-Medium"
+    static let semiBold = "Outfit-SemiBold"
+    static let bold = "Outfit-Bold"
+    static let extraBold = "Outfit-ExtraBold"
+}
+
+struct AppFontSizes {
+    static let largeTitle: CGFloat = 30
+    static let title1: CGFloat = 25
+    static let title2: CGFloat = 19
+    static let title3: CGFloat = 17
+    static let body: CGFloat = 14
+    static let footnote: CGFloat = 12
+    static let caption: CGFloat = 11
+}
