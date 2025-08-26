86f9829451dc38bee78b1751f8e518e6ee63613f
Enjel Hutasoit <enjel.hutasoit@gmail.com>
2025-08-26 18:42:02 +0700

    Add Injection class for dependency injection of game-related components

---
+++ GimOn/Core/DI/Injection.swift
@@ -0,0 +1,20 @@
+//
+//  Injection.swift
+//  GimOn
+//
+//  Created by Enjel Hutasoit on 26/08/25.
+//
+
+import Foundation
+
+final class Injection: NSObject {
+    
+    private func provideRepository() -> GameRepositoryProtocol {
+        let remote = GameRemoteDataSource.sharedInstance
+        return GameRepository(remote: remote)
+    }
+    
+    func provideGameListUseCase() -> GameListUseCase {
+        GameListInteractor(repository: provideRepository())
+    }
+}
